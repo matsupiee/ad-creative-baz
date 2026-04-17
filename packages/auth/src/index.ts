c06e262341e4ad70977e512f483d7ed9002333ab
matsupiee <btq32jh@icloud.com>
2026-04-18 00:26:47 +0900

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { createDb } from "@ad-creative-baz/db";
+import * as schema from "@ad-creative-baz/db/schema/auth";
+import { env } from "@ad-creative-baz/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "sqlite",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies()],
+  });
+}
